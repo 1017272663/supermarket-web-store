7bf9451e6d0fd2ce8eb193fc6cc4a925bcd3d876
unknown <j.alvarez2744@pascualbravo.edu.co>
2020-10-07 10:58:22 -0500

    Se realizo todo lo indicado en la clase el dia 29 sep, se agragaron las dependencias de springBoot y los driver de la base de datos mariaDb indicados en clase, tambien se creo una conecion exitosa con la base de datos localhost:3306 de mariaDB

---
+++ WebMiniMercadoLosPacos/src/main/java/com/minimercadolospacos/webminimercadolospacos/WebApplicaion.java
@@ -0,0 +1,12 @@
+package com.minimercadolospacos.webminimercadolospacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class WebApplicaion {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicaion.class, args);
+    }
+}
